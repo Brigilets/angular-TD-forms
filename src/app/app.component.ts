3450a0723113ac0a67c7972e9e8952a9f79bd379
Brigilets <b.sabutyte@gmail.com>
2023-08-03 09:25:20 +0200

    implemented  sugestUsername method using patchValue() method and tried the example of setValue() method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,29 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  genders = ['male', 'female', 'non-binary'];
   //remove f from onSubmit() in the template if using @ViewChild()
   @ViewChild('f') signupForm: NgForm;
+
   defaultQuestion = 'pet';
   answer: string = '';
+  genders: string[] = ['male', 'female', 'non-binary'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // Overrides the already filled in values,dus not ideal
+
+    // this.signupForm.setValue({
+    //   userdata: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: '',
+    // });
+
+    // Allows us to override precise value
+    this.signupForm.form.patchValue({ userdata: { username: suggestedName } });
   }
 
   // onSubmit(form: NgForm) {
